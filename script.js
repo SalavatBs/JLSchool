3c6dd543c1385a328e4e7315275624af5877ce5a
SalavatBs <78024432+SalavatBs@users.noreply.github.com>
2021-03-25 17:37:07 +0500

    done

--- script.js
+++ script.js
@@ -2,14 +2,7 @@
 // for ( var i = 0, len = elms.length; i < len; i++ ) {
 // 	new Splide( elms[ i ] ).mount();
 // }
-new Splide( '.splide.swiper-container--intro', {
-	perPage: 3,
-	rewind : true,
-	classes: {
-		prev  : 'splide__arrow--prev your-class-prev',
-		next  : 'splide__arrow--next your-class-next',
-	}
-} ).mount();
+new Splide( '.splide.swiper-container--intro' ).mount();
 
 new Splide( '.splide.swiper-container--reviews', {
 	type   : 'loop',
